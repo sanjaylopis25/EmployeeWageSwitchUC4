023c118cf6693bc467a27debcfb29194dc6d44e5
SANJAY <SANJAY@DESKTOP-OVOTIDF>
2021-10-25 00:20:48 +0100

    Add project files.

---
+++ EmployeeWageSwitchUC4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EmployeeWageSwitchUC4
+{
+    class Program
+    {
+        public const int Fulltime = 1;
+        public const int Parttime = 2;
+        public const int Emprateperhr = 20;
+        static void Main(string[] args)
+        {
+           
+
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            switch (empcheck)
+            {
+                case Fulltime:
+                    emphrs = 8;
+                    break;
+                case Parttime:
+                    emphrs = 4;
+                    break;
+                default:
+                    emphrs = 0;
+                    break;
+            }
+ 
+            empwage = emphrs * Emprateperhr;
+            Console.WriteLine("Employee Wage : " + empwage);
+            
+        }
+    }
+}
